ec4afceda332cbb39c91e35be449c6be41360c5f
Nair_Ishani <ishanimnair@gmail.com>
2026-03-04 11:23:16 +0530

    Setup basic game state and DOM references

---
+++ script.js
@@ -0,0 +1,11 @@
+let userScore=0;
+let computerScore=0;
+let round=1;
+
+const choices=["rock", "paper","scissors"];
+
+const roundDisplay=document.getElementById("roundDisplay");
+const countdownEl=document.getElementById("countdownEl");
+const roundResult=document.getElementById("roundResult");
+
+const buttons=document.querySelectorAll(".btn");
